b434467162443c1eba8ed9f2f886f915ec19cac0
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-02-17 19:49:10 -0500

    Added apadtive entropic descent and adaptive SGD

--- maxwell_d/optimizers.py
+++ maxwell_d/optimizers.py
@@ -7,7 +7,7 @@ def sgd(grad, x, v=None, callback=None, iters=200, learn_rate=0.1, decay=0.9):
     if v is None: v = np.zeros(len(x))
     for t in xrange(iters):
         g = grad(x, t)
-        if callback: callback(x, t, g)
+        if callback: callback(x, t, g, v)
         v = v - g
         x += learn_rate * v
         v *= decay
@@ -21,10 +21,46 @@ def entropic_descent(grad, x, v=None, callback=None, iters=200, learn_rate=0.1, 
     if rs is None: rs = npr.RandomState(0)
     for t in xrange(iters):
         g = grad(x, t)
-        if callback: callback(x, v, t, g)
+        if callback: callback(x, t, g, v)
         v = v - g
         x += learn_rate * v
         r = rs.randn(len(v))
         v = v * np.cos(theta) + np.sin(theta) * r * norm(v) / norm(r)  # This is not quite correct yet.
         v *= decay
+    return x
+
+
+def adaptive_entropic_descent(grad, x, v=None, callback=None, iters=200,
+                              init_learn_rate=0.1, init_log_decay=np.log(0.9),
+                              meta_learn_rate=0.01, meta_decay=0.01):
+    """Stochastic gradient descent with momentum with auto-adapting learning rate and decay.
+    Hyperparameters are updated using gradient descent with stepsizes given by the meta params."""
+    if v is None: v = np.zeros(len(x))
+    learn_rates = np.full(len(x), init_learn_rate)
+    log_decays = np.full(len(x), init_log_decay)
+    for t in xrange(iters):
+        g = grad(x, t)
+        if callback: callback(x, t, g, v)
+        v = v * np.exp(log_decays) - g
+        log_decays += meta_decay * (learn_rates * ( v + g ) + 1)
+        x += learn_rates * v
+        learn_rates += meta_learn_rate * g * v
+    return x
+
+
+def adaptive_sgd(grad, x, v=None, callback=None, iters=200,
+                              init_learn_rate=0.1, init_log_decay=np.log(0.9),
+                              meta_learn_rate=0.01, meta_decay=0.01):
+    """Stochastic gradient descent with momentum with auto-adapting learning rate and decay.
+    Hyperparameters are updated using gradient descent with stepsizes given by the meta params."""
+    if v is None: v = np.zeros(len(x))
+    learn_rates = np.full(len(x), init_learn_rate)
+    log_decays = np.full(len(x), init_log_decay)
+    for t in xrange(iters):
+        g = grad(x, t)
+        if callback: callback(x, t, g, v)
+        v = v * np.exp(log_decays) - g
+        log_decays += meta_decay * (learn_rates * ( v + g ))
+        x += learn_rates * v
+        learn_rates += meta_learn_rate * g * v
     return x
