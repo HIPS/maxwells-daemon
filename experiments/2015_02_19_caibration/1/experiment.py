9877ca8997271073244f01822eb5edf333e4e7d0
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-02-19 21:21:56 -0500

    Added v2 of entropic descent, and a simple experiment to test its calibration

---
+++ experiments/2015_02_19_caibration/1/experiment.py
@@ -0,0 +1,82 @@
+"""Some simple diagnostic tests to check whether entropic descent is working."""
+
+import numpy as np
+import pickle
+from collections import defaultdict
+
+from funkyyak import grad
+
+from maxwell_d.util import RandomState
+from maxwell_d.optimizers import entropic_descent2
+
+# ------ Problem parameters -------
+nllfun = lambda x: -0.5 * np.log(2.0*np.pi) + 0.5*np.sum(x**2)  # Marg lik should be 1
+nllfunt = lambda x, t : nllfun(x)
+gradfun = grad(nllfunt)
+
+# ------ Variational parameters -------
+seed = 0
+x_init_scale = np.full(1, 10.0)
+epsilon = 0.11
+gamma = 0.01
+N_iter = 200
+
+# ------ Plot parameters -------
+N_samples = 5
+
+def run():
+    print "Running experiment..."
+
+    results = defaultdict(list)
+    for i in xrange(N_samples):
+        def callback(x, t, g, v, entropy):
+            results[("x", i, t)] = x.copy()    # Replace this with a loop over kwargs?
+            results[("entropy", i, t)] = entropy
+            results[("velocity", i, t)] = v
+            results[("likelihood", i, t)] = -nllfun(x)
+
+        rs = RandomState((seed, i))
+        x, entropy = entropic_descent2(gradfun, callback=callback, x_scale=x_init_scale,
+                                       epsilon=epsilon, gamma=gamma, iters=N_iter, rs=rs)
+        results[("x", i, N_iter)] = x
+        results[("entropy", i, N_iter)] = x
+
+    return results
+
+
+def plot():
+    print "Plotting results..."
+    import matplotlib.pyplot as plt
+    with open('results.pkl') as f:
+          results = pickle.load(f)
+
+    fig = plt.figure(0); fig.clf()
+    ax = fig.add_subplot(111)
+    for i in xrange(N_samples):
+        plt.plot([results[("x", i, t)] for t in xrange(N_iter)])
+    plt.savefig("paths.png")
+
+    fig = plt.figure(0); fig.clf()
+    ax = fig.add_subplot(111)
+    for i in xrange(N_samples):
+        plt.plot([results[("entropy", i, t)] for t in xrange(N_iter)])
+    plt.savefig("entropy.png")
+
+    fig = plt.figure(0); fig.clf()
+    ax = fig.add_subplot(111)
+    for i in xrange(N_samples):
+        plt.plot([results[("likelihood", i, t)] for t in xrange(N_iter)])
+    plt.savefig("likelihoods.png")
+
+    fig = plt.figure(0); fig.clf()
+    ax = fig.add_subplot(111)
+    for i in xrange(N_samples):
+        plt.plot([results[("likelihood", i, t)] + results[("entropy", i, t)] for t in xrange(N_iter)])
+    plt.savefig("marginal_likelihoods.png")
+
+
+if __name__ == '__main__':
+    results = run()
+    with open('results.pkl', 'w') as f:
+        pickle.dump(results, f, 1)
+    plot()
--- maxwell_d/optimizers.py
+++ maxwell_d/optimizers.py
@@ -64,3 +64,32 @@ def adaptive_sgd(grad, x, v=None, callback=None, iters=200,
         x += learn_rates * v
         learn_rates += meta_learn_rate * g * v
     return x
+
+
+def entropic_descent2(grad, x_scale, callback=None, iters=200, epsilon=0.1, gamma=0.1, rs=None):
+    """Stochastic gradient descent with momentum and velocity randomization.
+       gamma controls the amount velocities randomize each iteration.
+       epsilon is roughly the scale of the square root of
+       the largest eigenvalue of the Hessian of the log-posterior at the mode.
+       rs is a RandomState."""
+    alpha = 0.1  # integration step-size.
+    D = len(x_scale)
+    annealing_schedule = np.linspace(0,1,iters)
+    x = rs.randn(D) * x_scale
+    v = rs.randn(D)
+    x_entropy = 0.5 * D * np.log(2*np.pi) + 0.5 * np.sum(np.log(x_scale))
+    v_entropy = 0.5 * D * np.log(2*np.pi) + 0.5 * D
+    entropy = x_entropy + v_entropy
+    for t, anneal in enumerate(annealing_schedule):
+        cur_epsilon = anneal * epsilon + (1 - anneal) * x_scale
+        neg_dlog_init = x / x_scale
+        g = anneal * grad(x, t) + (1 - anneal) * neg_dlog_init
+        if callback: callback(x, t, g, v, entropy)
+        v = v - cur_epsilon * alpha * g
+        x += cur_epsilon * alpha * v
+        r = rs.randn(len(v))
+        old_v_norm = norm(v)  # Track how much we grow or shrink due to r.
+        v = v * np.sqrt(1-gamma**2) + gamma * r
+        new_v_norm = norm(v)
+        entropy += 0.5*np.log(new_v_norm) - 0.5*np.log(old_v_norm)
+    return x, entropy
